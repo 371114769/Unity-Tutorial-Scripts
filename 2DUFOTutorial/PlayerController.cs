f67bf7b682815e6037eee9267a6c982799be48fb
Choi Chiu Kei <45446161+371114769@users.noreply.github.com>
2019-11-13 12:05:57 +0800

    Add files via upload

---
+++ 2DUFOTutorial/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text scoretext;
+    public Text winText;
+
+    private int score = 0;
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        UpdateScore();
+        winText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("PickUp"))
+        {
+            Destroy(other.gameObject);
+            score += 1;
+            UpdateScore();
+        }
+        if (score >= 7)
+        {
+            winText.text = "You Win!!!";
+        }
+    }
+
+    void UpdateScore()
+    {
+        scoretext.text = "Score: " + score;
+    }
+
+
+}
